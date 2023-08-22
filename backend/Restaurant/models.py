4fab64a688369f38a3648ae00cc9b774ba69a1b6
shao <junlin5525@gmail.com>
2023-08-22 17:24:42 +0000

    fix: 🐛 add comment user

--- backend/Restaurant/models.py
+++ backend/Restaurant/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-
+from Custom_User.models import CustomUser
 # Create your models here.
 
 
@@ -37,6 +37,8 @@ class Restaurants(models.Model):
 
 
 class Comment(models.Model):
+    user = models.ForeignKey(CustomUser,  null=False,
+                             on_delete=models.CASCADE)
     Restaurant = models.ForeignKey(Restaurants, on_delete=models.CASCADE)
     Rating = models.IntegerField()
     Title = models.CharField(max_length=120)
@@ -45,4 +47,4 @@ class Comment(models.Model):
         upload_to='Restaurant/Comment/', blank=True, null=True)
 
     def __str__(self):
-        return f"❰{self.Restaurant}❱- ❰{self.Title}❱"
+        return f"<{self.user}>-❰{self.Restaurant}❱- ❰{self.Title}❱"
