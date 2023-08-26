ff6d0e9219880a95c786c25f42bb1705fc549765
shao <junlin5525@gmail.com>
2023-08-26 16:05:45 +0000

    fix: 🐛 update testing problem

--- backend/Restaurant/tests.py
+++ backend/Restaurant/tests.py
@@ -24,7 +24,6 @@ class APITestCase(TestCase):
         )
         self.token = create_access_token(self.user)
         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(self.token))
-        print(self.token)
         self.restaurant = Restaurants.objects.create(
             Name='Test Restaurant',
             Country='Test Country',
@@ -36,17 +35,17 @@ class APITestCase(TestCase):
         )
 
         self.comment = Comment.objects.create(
-            user_pk=self.user,
-            Restaurant=self.restaurant,
             Rating=5,
             Title='Test Comment',
-            Body='This is a test comment'
+            Body='This is a test comment',
+            user_pk=self.user,
+            Restaurant=self.restaurant,
         )
 
     def test_restaurant_list(self):
         url = reverse('Restaurant-List')
         response = self.client.get(url)
-        print(response.data)
+        # print(response.data)
         restaurants = Restaurants.objects.all()
         serializer = RestaurantSerializer(restaurants, many=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -55,24 +54,87 @@ class APITestCase(TestCase):
     def test_restaurant_detail(self):
         url = reverse('Restaurant_Detail', kwargs={'pk': self.restaurant.pk})
         response = self.client.get(url)
-        print(response.data)
+       # print(response.data)
         restaurant = Restaurants.objects.get(pk=self.restaurant.id)
         serializer = RestaurantSerializer(restaurant)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
 
-    # def test_comment_list(self):
-    #     url = reverse('Comment-Restaurant')
-    #     response = self.client.get(url)
-    #     serializer = RestaurantCommentSerializer(Comment, many=True)
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertEqual(response.data, serializer.data)
+    def test_update_restaurant(self):
+        updated_data = {
+            'Name': 'Updated Restaurant Name',
+            'Country': 'Updated Country',
+            'Price': 150,
+            'Rating': 4,
+            'People': 10,
+            'Address': 'update adress',
+            'Introduction': 'update introduction'
+        }
+        url = reverse('Restaurant_Detail', kwargs={'pk': self.restaurant.pk})
+        response = self.client.put(url, data=updated_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_restaurant = Restaurants.objects.get(pk=self.restaurant.id)
+        self.assertEqual(updated_restaurant.Name, updated_data['Name'])
+        self.assertEqual(updated_restaurant.Country, updated_data['Country'])
+        self.assertEqual(updated_restaurant.Price, updated_data['Price'])
+        self.assertEqual(updated_restaurant.Rating, updated_data['Rating'])
+        self.assertEqual(updated_restaurant.People, updated_data['People'])
+        self.assertEqual(updated_restaurant.Address, updated_data['Address'])
+        self.assertEqual(updated_restaurant.Introduction.strip(),
+                         updated_data['Introduction'].strip())
 
-    # def test_comment_detail(self):
-    #     url = reverse('Comment-Restaurant-Detail',
-    #                   kwargs={'pk': self.comment.id})
-    #     response = self.client.get(url)
-    #     comment = Comment.objects.get(pk=self.comment.id)
-    #     serializer = RestaurantCommentSerializer(Comment)
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertEqual(response.data, serializer.data)
+    def test_delete_restaurant(self):
+        url = reverse('Restaurant_Detail', kwargs={'pk': self.restaurant.pk})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        with self.assertRaises(Restaurants.DoesNotExist):
+            deleted_restaurant = Restaurants.objects.get(pk=self.restaurant.pk)
+
+    def test_comment_list(self):
+        url = reverse('Comment-Restaurant')
+        response = self.client.get(url)
+        # print(response.data)
+        comment = Comment.objects.all()
+        serializer = RestaurantCommentSerializer(comment, many=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_comment_detail(self):
+        url = reverse('Comment-Restaurant-Detail',
+                      kwargs={'pk': self.comment.id})
+        response = self.client.get(url)
+        # print(response.data)
+        comment = Comment.objects.get(pk=self.restaurant.id)
+        serializer = RestaurantCommentSerializer(comment)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_update_comment(self):
+        updated_data = {
+            'Rating': 4,
+            'Title': 'update title',
+            'Body': 'update body',
+            'user_pk': self.user.pk,
+            'Restaurant': self.restaurant.pk
+        }
+        url = reverse('Comment-Restaurant-Detail',
+                      kwargs={'pk': self.comment.id})
+        response = self.client.put(url, data=updated_data)
+        # print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        comment = Comment.objects.get(pk=self.comment.id)
+
+        self.assertEqual(comment.Rating, updated_data['Rating'])
+        self.assertEqual(comment.Title.strip(), updated_data['Title'].strip())
+        self.assertEqual(comment.Body.strip(), updated_data['Body'].strip())
+    #    self.assertEqual(comment.user_pk, updated_data['user_pk'])
+
+    def test_delete_comment(self):
+        url = reverse('Comment-Restaurant-Detail',
+                      kwargs={'pk': self.restaurant.id})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        with self.assertRaises(Comment.DoesNotExist):
+            deleted_comment = Comment.objects.get(pk=self.comment.id)
