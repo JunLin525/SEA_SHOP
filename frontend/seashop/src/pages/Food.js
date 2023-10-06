4f0d41b980e972cf9fc6078643b4c56ec0ee1482
junlin525 <junlin5525@gmail.com>
2023-10-06 02:37:30 +0000

    feat: 🎸 add username showing and fix some layout problem

--- frontend/seashop/src/pages/Food.js
+++ frontend/seashop/src/pages/Food.js
@@ -42,22 +42,22 @@ function Food() {
                     <div className='food-content'>
                         <h1 className='food__title'>東南亞美食介紹</h1>
                         <p calssName='food__subtitle'>這是一個分享東南亞聚落地景的頁面，
-                            <br />歡迎分享你所知所了解的區域，讓更多喜愛嘗鮮的人們前往。</p>
+                            <br />歡迎登入並留言分享你對下面區域認識，讓更多喜愛嘗鮮的人們前往。</p>
                     </div>
                     <div className="cards">
                         <ul>
                             {food.map(item => (
                                 <div >
-                                    <li className='book_item' key={item.pk}>
+                                    <li className='book_item' key={item.id}>
                                         <article class="cardd">
                                             <div className='card__like'>
                                                 <div className='book_info'>
                                                     <img className="card__img" src={item.Picture} alt="Book Cover" style={{ width: '200px', heigh: '200px' }} />
                                                     <br />
-                                                    <Link to={`/Food-detail/${item.pk}`} className="card_link"> {item.AreaName}</Link>
+                                                    <Link to={`/Food-detail/${item.id}`} className="card_link"> {item.AreaName}</Link>
                                                     <div className="card__info">
                                                         <div className='author'>地址：{item.Address}</div>
-                                                        <div classNmae='publisher'>交通方式:{item.PublicTransportation}</div>
+                                                        <div classNmae='publisher'>交通方式：{item.PublicTransportation}</div>
                                                         <div className='publisher'>國家：{item.Country}</div>
                                                         <div className='ISBN'>介紹：{item.Introduction}</div>
                                                         <br />
--- frontend/seashop/src/pages/FoodDetail.js
+++ frontend/seashop/src/pages/FoodDetail.js
@@ -10,6 +10,7 @@ function FoodDetail() {
     const BASE_URL = "https://junlin5525.dev/api"
     const navigate = useNavigate()
     const { foodID } = useParams()
+    const [comments, setComments] = useState([])
     const [food, setFood] = useState([])
     const authTokens = JSON.parse(localStorage.getItem('authTokens')); // 從 localStorage 中獲取 Access Token
 
@@ -17,7 +18,7 @@ function FoodDetail() {
     useEffect(() => {
         let getFood = async () => {
 
-            const response = await fetch(`${BASE_URL}/api/foodie-Area/Area-Detail/${foodID}`, {
+            const response = await fetch(`${BASE_URL}/Settlement-api/Detail/${foodID}`, {
 
                 method: 'GET',
                 headers: {
@@ -36,27 +37,138 @@ function FoodDetail() {
         getFood()
     }, [foodID])
 
+    /*get food comment list*/
+    useEffect(() => {
+        let getComment = async () => {
+            const authTokens = JSON.parse(localStorage.getItem('authTokens')); // 從 localStorage 中獲取 Access Token
+
+            const response = await fetch(`${BASE_URL}/Settlement-api/Reply-List`, {
+
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + String(authTokens.access)
+
+                },
+            })
+            const commentData = await response.json();
+            setComments(commentData);
+        };
+
+        getComment()
+    }, [])
+
+
+
+
+
+    /*新增留言的function*/
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const authTokens = JSON.parse(localStorage.getItem('authTokens')); // 從 localStorage 中獲取 Access Token
+        const formData = new FormData();
+        formData.append('Title', e.target.title.value);
+        formData.append('Body', e.target.comments.value);
+        formData.append('Area', food.id);
+        console.log(formData)
+        try {
+            const response = await fetch(`${BASE_URL}/Settlement-api/Reply-Detail/68466b4d-7f6f-4e77-b879-4a186cebaa45/`, {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + String(authTokens.access)
+                },
+                body: formData
+            });
+            if (response.ok) {
+                alert('Suceess submit!')
+                window.location.reload();
+                // 评论发送成功，执行相应的操作
+            } else {
+                // 评论发送失败，处理错误情况
+                alert('Failed to send comment');
+            }
+        } catch (error) {
+            console.error('Error sending comment:', error);
+        }
+    };
+
 
     return (
         <div className='landing-background'>
             <Header />
-            <div className='content'>
-                <br />
-                <h1>摘要區</h1>
-                <h4>{food.Name}</h4>
-                <h4>{food.Address}</h4>
-                <h4>{food.Country}</h4>
-                <h4>{food.Introduction}</h4>
-                <img src={food.Picture} alt="Book Cover" style={{ width: '200px', heigh: '200px' }} />
-                <br />
-                <hr />
-                <h2>留言區</h2>
+            <div className='white-mock'>
+                <div className='food-page'>
+                    <div className="cards">
 
+                        <div className='shop__like'>
+                            <h1>美食聚落</h1>
+                            <h2>{food.AreaName}</h2>
+                            <img src={food.Picture} alt="Book Cover" style={{ width: '200px', heigh: '200px' }} />
+                            <h4>聚落名稱：{food.Name}</h4>
+                            <h4>所在地址：{food.Address}</h4>
+                            <h4>代表國家：{food.Country}</h4>
+                            <h4>聚落介紹：{food.Introduction}</h4>
+                            <br />
+
+                            <div className='comment-color'>
+
+                                <h2>留言區</h2>
+                                <hr />
+                                <ul>
+                                    {comments.map((comment, index) => {
+                                        if (food.id === comment.Area) {
+                                            return (
+                                                <li key={comment.id}>
+                                                    <h4>貼文者：{comment.UserName}</h4>
+                                                    <h4><Link to={`/FoodCommentDetail/${comment.id}`}>{comment.AreaName}</Link></h4>
+                                                    <h4>{comment.Title}</h4>
+                                                    <h5>{comment.Body}</h5>
+                                                    <hr />
+
+                                                </li>
+                                            )
+                                        } else {
+                                            return "此處尚沒有留言，您可以新增留言成為第一則。"
+                                        }
+                                    })}
+                                </ul>
+
+
+
+
+                                <div className="add-comment__like">
+                                    <h3>新增留言</h3>
+                                    <form onSubmit={handleSubmit} >
+                                        <label htmlFor="title">留言標題：</label><br />
+                                        <input
+                                            type="text"
+                                            id="title"
+                                            name="title"
+                                            placeholder="新增留言標題"
+                                        /><br />
+                                        <label htmlFor="comments">留言評論:</label><br />
+                                        <input
+                                            type="text"
+                                            id="comments"
+                                            name="comments"
+                                            placeholder="新增留言評論"
+                                        /><br />
+                                        <br />
+                                        <button className='submit' type="submit">送出</button>
+                                    </form>
+                                    <br />
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+
+                    <div className='landing-back' />
+                    <Footer />
+                </div>
             </div>
-
-
-            <div className='landing-back' />
-            <Footer />
         </div>
 
     )
